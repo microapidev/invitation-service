12adf146b1af331d1f7a2ff92d1aa38e9b2ec714
Simeon <adegbolasimeon@gmail.com>
2020-06-08 20:36:26 +0100

    add getting all pending invite stub

--- services/store.js
+++ services/store.js
@@ -1,9 +1,11 @@
 const add = async (user, code) => {};
 const verify = async (user, code) => {};
 const remove = async (user) => {};
+const getAll = async () => {};
 
 module.exports = {
   add,
   verify,
   remove,
+  getAll,
 };
