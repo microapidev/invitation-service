aa9a9c4bfd9a3f86301cb48671d31eb03b996bfa
Simeon <adegbolasimeon@gmail.com>
2020-06-10 10:39:06 +0100

    implement invitation store

--- services/store.js
+++ services/store.js
@@ -1,7 +1,45 @@
-const add = async (user, code) => {};
-const verify = async (user, code) => {};
-const remove = async (user) => {};
-const getAll = async () => {};
+const { promisify } = require("util");
+
+const redis = require("redis");
+const client = redis.createClient();
+
+client.on("connect", () => {
+  console.log("Database successfully connected");
+});
+client.on("error", () => {
+  console.log("Error connecting to the database");
+});
+
+let hmset = promisify(client.hmset).bind(client);
+let keys = promisify(client.keys).bind(client);
+let hgetall = promisify(client.hgetall).bind(client);
+let del = promisify(client.del).bind(client);
+
+const add = async (user, code) => {
+  await hmset(code, user);
+};
+
+const verify = async (email, code) => {
+  let result = await hgetall(code);
+  if (result && result.email === email) {
+    return { name: result.name };
+  }
+  return null;
+};
+
+const remove = async (code) => {
+  await del(code);
+};
+
+const getAll = async () => {
+  let allKeys = await keys("*");
+  let allValues = [];
+  for (let key of allKeys) {
+    value = await hgetall(key);
+    allValues.push(value);
+  }
+  return allValues;
+};
 
 module.exports = {
   add,
