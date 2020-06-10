22b343ea5adc0de25fef932899ac3ad6cdb74c18
Simeon <adegbolasimeon@gmail.com>
2020-06-10 10:38:28 +0100

    implement mail sending

--- services/mail.js
+++ services/mail.js
@@ -1,3 +1,50 @@
-const send = async (message) => {};
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+
+const OAuth2 = google.auth.OAuth2;
+
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+const refreshToken = process.env.REFRESH_TOKEN;
+
+const oauth2Client = new OAuth2(
+  clientId,
+  clientSecret,
+  "https://developers.google.com/oauthplayground" // Redirect URL
+);
+
+oauth2Client.setCredentials({
+  refresh_token: refreshToken,
+});
+
+const send = async (message, email) => {
+  const accessToken = await oauth2Client.getAccessToken();
+
+  let sender = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    tls: {
+      rejectUnauthorized: false,
+    },
+    auth: {
+      type: "OAuth2",
+      user: "hngteamsilver@gmail.com",
+      clientId,
+      clientSecret,
+      accessToken: accessToken.token,
+      refreshToken,
+    },
+  });
+
+  let response = await sender.sendMail({
+    from: "'Team Silver' <hngteamsilver@gmail.com>",
+    subject: "Invitation to Team Silver",
+    to: email,
+    text: message,
+  });
+
+  console.log(response);
+};
 
 module.exports = { send };
